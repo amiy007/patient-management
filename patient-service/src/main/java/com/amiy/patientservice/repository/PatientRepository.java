c58cb21e59d349fe45133146306958b265ecbf90
amit yadav <uk08109@gmail.com>
2026-01-21 22:35:02 +0530

    initial commit

---
+++ patient-service/src/main/java/com/amiy/patientservice/repository/PatientRepository.java
@@ -0,0 +1,11 @@
+package com.amiy.patientservice.repository;
+
+import com.amiy.patientservice.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+}
